dbb9607daa1bf28f1e50c3fde539e384cefc449c
Marguerite <margueritebuisson7@gmail.com>
2019-09-11 15:42:53 +0200

    Ajout table experiences et formation Creation page de formulaire experiences (a faire encore)

--- initdb/1_TABLES.sql
+++ initdb/1_TABLES.sql
@@ -1,12 +1,46 @@
-create table users
+create table experiences
 (
-    id bigint auto_increment,
-    constraint users_pk
-        primary key (id),
-    first_name TEXT not null,
-    last_name TEXT not null,
-    age int null
+    experience_id  int auto_increment
+        primary key,
+    new_column     int        null,
+    jobTitle       text       null,
+    company        text       null,
+    contractType   text       null,
+    jobDescription text       null,
+    begYear        int        null,
+    endYear        int        null,
+    Ended          tinyint(1) null comment 'Si vrai terminé, si faux en cours
+'
 );
 
+create table formations
+(
+    formation_id   int auto_increment
+        primary key,
+    formation_name text       null,
+    despcription   text       null,
+    level          linestring null comment 'Mettre une liste BAC +1,2,3...',
+    city           text       null,
+    beg_year       date       null,
+    end_year       date       null,
+    ended          tinyint(1) null comment 'Si Vrai formation finie sinon en cours
+la date de fin correspond a la date supposée d''obtention du diplome
+'
+);
+
+create table users
+(
+    id          bigint auto_increment
+        primary key,
+    first_name  text       not null,
+    last_name   text       not null,
+    address     text       null,
+    city        text       null,
+    zip_code    int        null,
+    mail        text       null,
+    mobilePhone linestring null,
+    picture     text       null,
+    birthDate   date       null
+);
 
 
--- src/main/java/fr/takima/demo/User.kt
+++ src/main/java/fr/takima/demo/User.kt
@@ -10,8 +10,7 @@ data class User(
                 @GeneratedValue(strategy = GenerationType.IDENTITY)
                 @Id var id: Long?,
                 @Column(name = "first_name") var firstName: String?,
-                @Column(name = "last_name") var lastName: String?,
-                @Column(name = "age") var age: Int?){
-    constructor() : this(null, null, null, null)
-
+                @Column(name = "last_name") var lastName: String?)
+{
+    constructor() : this(null, null, null)
 }
