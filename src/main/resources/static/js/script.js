1259c3395e0ece5a8924f03df7840ece6944c847
fduchet <ferdinand.duchet-suchaux@epfedu.fr>
2019-09-23 19:04:59 +0200

    on affiche la date de fin d'une forma/experience pro si user dit que ce n'est pas son poste actuel

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -9,7 +9,18 @@ function toast() {
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
 
+// permet de copier un div
 function cloning(){
     var copie = $('#formulaire').clone();
     copie.appendTo('body');
 }
+
+// permet d'afficher un div
+function affiche(val) {
+    document.getElementById("endedornot").disabled=true;
+    if(val==0)
+        document.getElementsByClassName("actuel")[0].style.visibility="hidden"
+    if(val==1)
+        document.getElementsByClassName("actuel")[0].style.visibility="visible"
+}
+
